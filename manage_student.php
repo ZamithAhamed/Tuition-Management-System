fa7eb40408f315367de36208d3fe5ae43259ad37
ZamithAhamed <zamith11@gmail.com>
2022-11-13 22:59:27 +0530

    Validations added

--- manage_student.php
+++ manage_student.php
@@ -12,27 +12,65 @@ foreach($qry->fetch_array() as $k => $val){
         <input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
         <div id="msg" class="form-group"></div>
         <div class="form-group">
-            <label for="" class="control-label">Id No.</label>
-            <input type="text" class="form-control" name="id_no"  value="<?php echo isset($id_no) ? $id_no :'' ?>" required>
+            <label for="id_no" class="control-label">Id No.</label>
+            <input id="id_no"type="text" class="form-control" name="id_no"  value="<?php echo isset($id_no) ? $id_no :'' ?>" required>
         </div>
         <div class="form-group">
-            <label for="" class="control-label">Name</label>
-            <input type="text" class="form-control" name="name"  value="<?php echo isset($name) ? $name :'' ?>" required>
+            <label for="name" class="control-label">Name</label>
+            <input id="name"type="text" class="form-control" name="name"  value="<?php echo isset($name) ? $name :'' ?>" required>
         </div>
         <div class="form-group">
-            <label for="" class="control-label">Contact</label>
-            <input type="text" class="form-control" name="contact"  value="<?php echo isset($contact) ? $contact :'' ?>" required>
+            <label for="contact" class="control-label">Contact</label>
+            <input id="contact" type="text" class="form-control" name="contact"  value="<?php echo isset($contact) ? $contact :'' ?>" required>
         </div>
         <div class="form-group">
-            <label for="" class="control-label">Email</label>
-            <input type="email" class="form-control" name="email"  value="<?php echo isset($email) ? $email :'' ?>" required>
+            <label for="email" class="control-label">Email</label>
+            <input id="email" type="email" class="form-control" name="email"  value="<?php echo isset($email) ? $email :'' ?>" required>
         </div>
         <div class="form-group">
-            <label for="" class="control-label">Address</label>
-            <textarea name="address" id="" cols="30" rows="3" class="form-control" required=""><?php echo isset($address) ? $address :'' ?></textarea>
+            <label for="address" class="control-label">Address</label>
+            <textarea id="address"name="address" id="" cols="30" rows="3" class="form-control" required><?php echo isset($address) ? $address :'' ?></textarea>
         </div>
     </form>
 </div>
+
+<script type="text/javascript">
+    function validate(){
+        //Contact Validation
+        if(document.getElementById("id_no").value == ""){
+            alert_toast("Please Enter ID Number",'warning')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("name").value == ""){
+            alert_toast("Please Enter Name",'warning')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("contact").value == ""){
+            alert_toast("Please Enter Contact Number",'warning')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("email").value == ""){
+            alert_toast("Please Enter E-mail",'warning')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("address").value == ""){
+            alert_toast("Please Enter Address",'warning')
+            end_load()
+            return false;
+        }
+
+    }
+
+</script>
+
 <script>
     $('#manage-student').on('reset',function(){
         $('#msg').html('')
@@ -42,6 +80,63 @@ foreach($qry->fetch_array() as $k => $val){
         e.preventDefault()
         start_load()
         $('#msg').html('')
+
+
+        if(document.getElementById("id_no").value == ""){
+            alert_toast("Please Enter ID Number",'warning')
+            end_load()
+            return false;
+        }
+
+        var regex = /^[0-9]+$/ ;
+        if(!regex.test(document.getElementById("id_no").value)){
+            alert_toast("Invalid ID Number",'danger')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("name").value == ""){
+            alert_toast("Please Enter Name",'warning')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("contact").value == ""){
+            alert_toast("Please Enter Contact Number",'warning')
+            end_load()
+            return false;
+        }
+
+    
+        //contact validation
+        var regex = /^[0][0-9]{9}$/ ;
+        if(!regex.test(document.getElementById("contact").value)){
+            alert_toast("Invalid Contact Number",'danger')
+            end_load()
+            return false;
+        }
+
+        if(document.getElementById("email").value == ""){
+            alert_toast("Please Enter E-mail",'warning')
+            end_load()
+            return false;
+        }
+        
+        //emial validation
+        var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+        if(!regex.test(document.getElementById("email").value)){
+                alert_toast("Invalid Email",'danger')
+                end_load()
+                return false;
+        }
+
+        if(document.getElementById("address").value == ""){
+            alert_toast("Please Enter Address",'warning')
+            end_load()
+            return false;
+        }
+
+
         $.ajax({
             url:'ajax.php?action=save_student',
             data: new FormData($(this)[0]),
